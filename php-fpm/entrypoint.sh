2e654d4787d49f6bed89862656c88237602c300c
kurofuku <kurofuku98@hotmail.com>
2021-10-11 22:34:54 +0900

    Some improvement.

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -x
 if [ ! -f ${NGINX_ROOT}/index.php ]; then
     cd  /;
     unzip -q /nextcloud-${NEXTCLOUD_VER}.zip;
@@ -9,10 +9,29 @@ if [ ! -f ${NGINX_ROOT}/index.php ]; then
     rm /nextcloud-${NEXTCLOUD_VER}.zip;
     rm /config.php.template;
 fi
-# Replace environment variables to real value
-envsubst '$$PHP_VER' < /etc/php/${PHP_VER}/fpm/php-fpm.conf.template > /etc/php/${PHP_VER}/fpm/php-fpm.conf
-envsubst '$$PHP_VER$$NGINX_USER$$NGINX_GROUP$$PHP_SESSION_DIR' < /etc/php/${PHP_VER}/fpm/pool.d/www.conf.template > /etc/php/${PHP_VER}/fpm/pool.d/www.conf
+cat > ${PHP_INI_DIR}-fpm.d/zz-docker.conf << EOF
+[www]
+user = ${NGINX_USER}
+group = ${NGINX_GROUP}
+
+listen = /var/run/php-fpm.sock
+listen.owner = ${NGINX_USER}
+listen.group = ${NGINX_GROUP}
+listen.mode = 660
+
+;php_value[opcache.enable] = 1
+;php_value[opcache.enable_cli] = 1
+php_value[opcache.interned_strings_buffer] = 8
+php_value[opcache.max_accelerated_files] = 10000
+php_value[opcache.memory_consumption] = 128
+php_value[opcache.save_comments] = 1
+php_value[opcache.revalidate_freq] = 1
+php_value[opcache.file_cache] = /var/lib/php/opcache
+php_value[session.save_handler] = files
+php_value[session.save_path] = /${PHP_SESSION_DIR}
+;php_value[soap.wsdl_cache_dir] = /var/lib/php/wsdlcache
+EOF
 # Extend max_execution_time
-sed -i -e "s/max_execution_time = 30/max_execution_time = 180/" /etc/php/${PHP_VER}/fpm/php.ini
+sed -i -e "s/max_execution_time = 30/max_execution_time = 180/" ${PHP_INI_DIR}/php.ini
 # Run php-fpm
-/usr/sbin/php-fpm${PHP_VER} --nodaemonize --fpm-config /etc/php/${PHP_VER}/fpm/php-fpm.conf
+/usr/local/sbin/php-fpm --nodaemonize
