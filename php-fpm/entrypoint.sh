1e4ffcefeda8fb25fbe9d991036bb579f5ab9209
kurofuku <kurofuku98@hotmail.com>
2024-01-20 12:43:59 +0900

    Parameterized.

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-if [ ! -f ${NGINX_ROOT}/index.php ]; then
+if [ ! -f ${CONTAINER_WEBROOT_DIR}/index.php ]; then
     cd  /;
     unzip -q /nextcloud-${NEXTCLOUD_VER}.zip;
-    mv /nextcloud/* ${NGINX_ROOT};
-    envsubst '$$NGINX_SERVER_NAME' < /config.php.template > ${NGINX_ROOT}/config/config.php;
-    chown -R ${NGINX_USER}:${NGINX_GROUP} ${NGINX_ROOT};
+    mv /nextcloud/* ${{CONTAINER_WEBROOT_DIR};
+    envsubst '$$NGINX_SERVER_NAME' < /config.php.template > ${CONTAINER_WEBROOT_DIR}/config/config.php;
+    chown -R ${NGINX_USER}:${NGINX_GROUP} ${CONTAINER_WEBROOT_DIR};
     rm -rf /nextcloud;
     rm /nextcloud-${NEXTCLOUD_VER}.zip;
     rm /config.php.template;
