45955e4b58886e8040093f93b6dc4157a04d8493
kurofuku <kurofuku98@hotmail.com>
2025-03-20 10:16:06 +0900

    Small fixes.

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -2,7 +2,7 @@
 if [ ! -f ${CONTAINER_WEBROOT_DIR}/index.php ]; then
     cd  /;
     unzip -q /nextcloud-${NEXTCLOUD_VER}.zip;
-    mv /nextcloud/* ${{CONTAINER_WEBROOT_DIR};
+    mv /nextcloud/* ${CONTAINER_WEBROOT_DIR};
     envsubst '$$NGINX_SERVER_NAME' < /config.php.template > ${CONTAINER_WEBROOT_DIR}/config/config.php;
     chown -R ${NGINX_USER}:${NGINX_GROUP} ${CONTAINER_WEBROOT_DIR};
     rm -rf /nextcloud;
